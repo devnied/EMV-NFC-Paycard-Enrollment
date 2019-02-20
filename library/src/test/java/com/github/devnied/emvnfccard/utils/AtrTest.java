640209c3f89abf31189fb593ebce2dcfd09d95f8
devnied <mxjulien@gmail.com>
2019-02-20 18:06:27 +0100

    Update smartcard_list for atr and ats

--- library/src/test/java/com/github/devnied/emvnfccard/utils/AtrTest.java
+++ library/src/test/java/com/github/devnied/emvnfccard/utils/AtrTest.java
@@ -1,9 +1,10 @@
 package com.github.devnied.emvnfccard.utils;
 
-import java.util.Arrays;
-
 import org.fest.assertions.Assertions;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class AtrTest {
 
@@ -25,11 +26,19 @@ public class AtrTest {
 				"VISA card from Banque Populaire");
 		Assertions.assertThat(AtrUtils.getDescription(null)).isEqualTo(null);
 	}
-	
+
 	@Test
 	public void testDescriptionFromATS2() {
 
 		Assertions.assertThat(AtrUtils.getDescriptionFromAts("0F 55 B0 11 69 FF 4A 50 D0 80 00 49 54 03")).contains(
 				"Sky (Italy) VideoGuard CAM card");
 	}
+
+	@Test
+	public void testDescriptionFromATR_array() {
+		Assertions.assertThat(AtrUtils.getDescription("3F 65 35 10 00 04 6C")).isEqualTo(Collections.singletonList("Postcard (Switzerland)"));
+		Assertions.assertThat(AtrUtils.getDescription("3F 65 35 10 00 04 EC")).isEqualTo(Collections.singletonList("Postcard (Switzerland)"));
+		Assertions.assertThat(AtrUtils.getDescription("3F 65 35 10 02 04 EC")).isEqualTo(Collections.singletonList("Postcard (Switzerland)"));
+	}
+
 }
